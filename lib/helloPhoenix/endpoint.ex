dba416f8a5e69909c4a5b13474280bc66d9e7cae
vishal <vishal@intelltest.(none)>
2015-10-03 13:08:32 +0530

    initial commit

---
+++ lib/helloPhoenix/endpoint.ex
@@ -0,0 +1,39 @@
+defmodule HelloPhoenix.Endpoint do
+  use Phoenix.Endpoint, otp_app: :helloPhoenix
+
+  socket "/socket", HelloPhoenix.UserSocket
+
+  # Serve at "/" the static files from "priv/static" directory.
+  #
+  # You should set gzip to true if you are running phoenix.digest
+  # when deploying your static files in production.
+  plug Plug.Static,
+    at: "/", from: :helloPhoenix, gzip: false,
+    only: ~w(css fonts images js favicon.ico robots.txt)
+
+  # Code reloading can be explicitly enabled under the
+  # :code_reloader configuration of your endpoint.
+  if code_reloading? do
+    socket "/phoenix/live_reload/socket", Phoenix.LiveReloader.Socket
+    plug Phoenix.LiveReloader
+    plug Phoenix.CodeReloader
+  end
+
+  plug Plug.RequestId
+  plug Plug.Logger
+
+  plug Plug.Parsers,
+    parsers: [:urlencoded, :multipart, :json],
+    pass: ["*/*"],
+    json_decoder: Poison
+
+  plug Plug.MethodOverride
+  plug Plug.Head
+
+  plug Plug.Session,
+    store: :cookie,
+    key: "_helloPhoenix_key",
+    signing_salt: "B7gGQzbf"
+
+  plug HelloPhoenix.Router
+end
